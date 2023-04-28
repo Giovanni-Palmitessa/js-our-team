4020f908774acb0910202dd037c5d37cc57d1424
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-28 16:03:47 +0200

    add 2nd member

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 const teamMembers = [
     {
         nameSurname: 'Wayne Barnett', 
-        job: 'Chief Editor',
+        job: 'Founder & CEO',
         image: 'wayne-barnett-founder-ceo.jpg'
-    }
+    },
+
+    {
+        nameSurname: 'Angela Caroll', 
+        job: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },  
+    
+    
 ]
